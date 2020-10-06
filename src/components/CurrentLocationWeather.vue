5cdefa45e9c9ac1f86bc040bf706edc9d845204d
James Fraser <jimbob4t@virginmedia.com>
2020-10-06 13:59:46 +0100

    added sunset, sunrise, wind speed and temperature

--- src/components/CurrentLocationWeather.vue
+++ src/components/CurrentLocationWeather.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
-        <h2>The weather in your current location is: </h2>
-        <p v-if="this.currentCoordinates != null">Latitude: {{ this.currentCoordinates.latitude }}</p>
-        <p v-if="this.currentCoordinates != null">Longatude: {{ this.currentCoordinates.longitude }}</p>
-        <p v-if="this.currentLocationWeather != null">Current Weather: {{ this.currentLocationWeather.current.weather[0].description }}</p>
-        <p v-if="this.currentLocationDetails != null">Town: {{ this.currentLocationDetails.addresses[0].address.municipality }}</p>
-        <p v-if="this.currentLocationDetails != null">Post Code: {{ this.currentLocationDetails.addresses[0].address.postalCode }}</p>
+        <h2 v-if="this.currentLocationDetails != null" >The current weather in {{ this.currentLocationDetails.addresses[0].address.municipality }}, {{ this.currentLocationDetails.addresses[0].address.postalCode }} is : </h2>
+        <!-- <p v-if="this.currentCoordinates != null">Latitude: {{ this.currentCoordinates.latitude }}</p>
+        <p v-if="this.currentCoordinates != null">Longatude: {{ this.currentCoordinates.longitude }}</p> -->
+        <h2 v-if="this.currentLocationWeather != null"> {{ this.currentLocationWeather.current.weather[0].description }}</h2>
+        <img :src=this.weatherIcon class="image-fit">
+        
+        <h2 v-if="this.currentLocationWeather != null"> Temperature: {{ this.currentLocationWeather.current.temp  }}°C</h2>
+        <h2 v-if="this.currentLocationWeather != null"> Wind Speed: {{ this.currentLocationWeather.current.wind_speed  }}m/s</h2>
+        <h2 v-if="this.currentLocationWeather != null"> Sunrise: {{ this.convertTimeFromTimeStamp(this.currentLocationWeather.current.sunrise) }}</h2>
+        <h2 v-if="this.currentLocationWeather != null"> Sunset: {{ this.convertTimeFromTimeStamp(this.currentLocationWeather.current.sunset) }}</h2>
+        <!-- <p v-if="this.currentLocationDetails != null">Town: </p>
+        <p v-if="this.currentLocationDetails != null">Post Code: </p> -->
         <!-- <img src="http://openweathermap.org/img/wn/" +  {{this.currentLocationWeather.current.weather[0].icon}} + "@2x.png"> -->
-        <img :src=this.weatherIcon>
+        
     </div>
 </template>
 
@@ -30,7 +36,7 @@ export default {
         getCoordinates() {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition((position) => {
-                     fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&appid=' + apiKey.weatherKey )
+                     fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&appid=' + apiKey.weatherKey )
                     .then(res => res.json())
                     .then(results => this.currentLocationWeather = results)
                     .then((results) => this.weatherIcon = "http://openweathermap.org/img/wn/" +  results.current.weather[0].icon + "@2x.png")
@@ -49,6 +55,12 @@ export default {
             .then(results => this.currentLocationDetails = results)
         },
 
+        convertTimeFromTimeStamp(path) {
+            const date = new Date(path * 1000)
+            const time = date.toLocaleTimeString("en-US", {hour: "2-digit", minute: "2-digit"})
+            return time
+        }
+
 
 
     },
@@ -64,5 +76,8 @@ export default {
 </script>
 
 <style scoped>
-
+.image-fit{
+    height: 25%;
+    width: 25%;
+}
 </style>
