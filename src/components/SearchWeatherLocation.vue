a00a983c79c224aed001b04e58f0141e89190dc4
James Fraser <jimbob4t@virginmedia.com>
2020-11-17 12:22:58 +0000

    sorted a bug with the fetch always returning great britain as the location.

--- src/components/SearchWeatherLocation.vue
+++ src/components/SearchWeatherLocation.vue
@@ -1,5 +1,6 @@
 <template>
     <form @submit.prevent>
+        <label>Search a location: </label>
         <input v-model="searchTerm" />
         <button v-on:click="submit">Submit</button>
     </form>
@@ -21,9 +22,11 @@ export default {
   },
   methods: {
       submit() {
-            fetch("https://api.tomtom.com/search/2/structuredGeocode.json?countryCode=GB& municipalitySubdivision=" + this.searchTerm + "&key=" + apiKey.tomTomKey )
+            fetch("https://api.tomtom.com/search/2/structuredGeocode.json?countryCode=GB&municipalitySubdivision=" + this.searchTerm + "&key=" + apiKey.tomTomKey )
             .then(res => res.json())
-            .then(results => this.searchLocationResults = results)
+            .then(results => {
+                this.searchLocationResults = results
+            })
             .then(() => {
                 if(this.searchLocationResults.results.length === 0) {
                     alert("Please enter a valid location.")
@@ -34,12 +37,14 @@ export default {
           
       },
       conditionalFetch(){
+          console.log("searchLocationResults", this.searchLocationResults)
           fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + this.searchLocationResults.results[0].position.lat + '&lon=' + this.searchLocationResults.results[0].position.lon + '&units=metric&appid=' + apiKey.weatherKey )
           .then(res => res.json())
           .then(results => this.searchLocationWeather = results)
           .then((results) => this.weatherIcon = "http://openweathermap.org/img/wn/" +  results.current.weather[0].icon + "@2x.png")
           .then(() => eventBus.$emit("searched-weather-data", {searchLocationWeather: this.searchLocationWeather, searchLocationResults: this.searchLocationResults}))
       }
+
   }
 
 }
